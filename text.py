a16eb5763581530ee47c0633988a2398ea70793f
BenLucht <benjamin.b.lucht@gmail.com>
2017-12-28 09:28:28 +0000

    Add text module concept

---
+++ text.py
@@ -0,0 +1,22 @@
+class text():
+    def __init__(self):
+        """Reads text from a file, stores it and text statistics"""
+        pass
+
+    def count_words(self):
+        pass
+
+    def count_keystrokes(self):
+        pass
+
+    def count_characters(self):
+        pass
+
+    def distribution_characters(self):
+        pass
+
+    def distribution_words(self):
+        pass
+
+    def average_words(self):
+        pass
