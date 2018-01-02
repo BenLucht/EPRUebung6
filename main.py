df7a6f2d4e0702913dcec002976faff8b1126540
BenLucht <benjamin.b.lucht@gmail.com>
2018-01-02 20:32:57 +0000

    Add import and call to gui module

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Main Module for EPR6 exercises.
 """
 
 from text import text
+from text_statistics_interface import text_stat_window
 
 __author__ = "xxxxxxx: Ben, xxxxxxx: Anne"
 __copyright__ = "Copyright 2017/2018 – EPR-Goethe-Uni"
@@ -11,3 +12,5 @@ __email__ = "xxxxxxxx@stud.uni-frankfurt.de, xxxxxxxx@stud.uni-frankfurt.de"
 
 if __name__ == '__main__':
     morgen_kinder = text('src/Morgen_Kinder.txt')
+
+    #text_stat_window() #opens the tkinter interface
